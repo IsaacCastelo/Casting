1abce93bd2aadd7515f06c2480a327d132b0c97f
IsaacCastelo <isaacpxl8@gmail.com>
2022-05-10 07:27:27 -0700

    .
    .

--- CastingBD_DAO/src/Prueba/Prueba.java
+++ CastingBD_DAO/src/Prueba/Prueba.java
@@ -23,10 +23,8 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        IClienteDAO clienteDAO = new ClientesDAO(new ConexionBD());
-        
-        clienteDAO.agregar(new Cliente("Miguel Hidalgo", new Direccion("Antonio Caso", "23B", "Villa Itson"), "6441177349", Cliente.tipoActividad.MODA , new Persona("Don Pancho", "6419827643", new Direccion("Los arboles", "489y", "Mantecada"), "PELS021028HSRYPLA5")));
-
+        IClienteDAO clienteDAO = new ClientesDAO(new ConexionBD());      
+       // clienteDAO.agregar(new Cliente("Miguel Hidalgo", new Direccion("Antonio Caso", "23B", "Villa Itson"), "6441177349", Cliente.tipoActividad.MODA , new Persona("Don Pancho", "6419827643", new Direccion("Los arboles", "489y", "Mantecada"), "PELS021028HSRYPLA5")));
         clienteDAO.consultarTodos().forEach((cliente) -> { System.out.println(cliente);});
     }
     
